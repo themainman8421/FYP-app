b3f1f4c2845d58aff0c94ccbf0add294a5f3b0b0
James Behan <Jcbchow@hotmail.com>
2021-03-12 12:40:38 +0000

    added retrofit comments

--- Voting app/app/src/main/java/com/example/votingapp/Retrofit/RetrofitClient.java
+++ Voting app/app/src/main/java/com/example/votingapp/Retrofit/RetrofitClient.java
@@ -3,11 +3,13 @@ package com.example.votingapp.Retrofit;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+//this class allows for the creation of a retrofit client
 public class RetrofitClient {
     private static Retrofit instance;
     private static String BASE_URL =  "http://10.0.2.2:3000";
 //    private static String BASE_URL =  "http://46.101.60.72:3000";
 
+    //setting the url
     public static String getBASE_URL() {
         return BASE_URL;
     }
@@ -16,6 +18,7 @@ public class RetrofitClient {
         this.BASE_URL = BASE_URL;
     }
 
+    //getting a retrofit instance
     public static Retrofit getInstance(){
         if(instance == null){
             instance = new Retrofit.Builder()
--- Voting app/app/src/main/java/com/example/votingapp/Retrofit/RetrofitInterface.java
+++ Voting app/app/src/main/java/com/example/votingapp/Retrofit/RetrofitInterface.java
@@ -17,49 +17,60 @@ import retrofit2.http.QueryMap;
 
 public interface RetrofitInterface {
 
+    //allows a user to login
     @POST("/users/Login")
     Call<User> executeLogin(@Body HashMap<String, String> map);
 
+    //allows a user to login
     @POST("/users/Register")
     Call<Void> executeSignup(@Body HashMap<String, String> map);
 
-
+    //creates a new popular or majority poll
     @POST("/poll/createPluralityAndMajorityPoll")
     Call<Void> executenewPoll(@Body HashMap<String, String> map);
 
+    //creates a new ranked choice poll
     @POST("/poll/createAlternativeVotePoll")
     Call<Void> createAlternativeVotePoll(
             @Body HashMap<String, String> map);
 
+    //checks if the poll vode is in use
     @POST("/poll/pollInUse")
     Call<Void> pollInUse(@Body HashMap<String, String> map);
 
+    //checks if the user has already voted on the poll
     @POST("/poll/pollCode")
     Call<Void> executepollCode(@Body HashMap<String, String> map);
 
-//    @GET("/poll/getPoll")
-//    Call<List<Poll>> getPoll(@QueryMap HashMap<String, String> map);
+    //get the poll
     @GET("/poll/getPoll")
     Call<Poll> getPoll(@QueryMap HashMap<String, String> map);
 
+    //increases the vote of a popular or majority vote
     @POST("/poll/increaseVote")
     Call<Void> increaseVote(@QueryMap HashMap<String, String> map);
 
+    //inserts users options into ranked choice
     @PUT("/poll/AVInsert")
     Call<Void> increaseAVVote(@QueryMap HashMap<String, String> map);
 
+    //gets the results of a popular or majority vote
     @GET("/results/")
     Call<Poll> getResults(@QueryMap HashMap<String, String> map);
 
+    //getting results for alternative vote
     @GET("/results/avResult")
     Call<Poll> getAVResults(@QueryMap HashMap<String, String> map);
 
+    //adds the poll the user has voted on to their info
     @PUT("/poll/pollsVotedOn")
     Call<Void> addPollVotedOn(@QueryMap HashMap<String, String> map);
 
+    //gets all polls
     @GET("/poll/allPolls")
     Call<List<Poll>> getallPolls();
 
+    //gets all the users polls
     @GET("/poll/userPolls")
     Call<List<Poll>> getallUserPolls(@QueryMap HashMap<String, String> map);
 
